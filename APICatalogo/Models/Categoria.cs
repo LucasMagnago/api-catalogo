8ef24ae9670fbae0aace62292323ff81a87f8896
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2023-02-26 10:35:45 -0300

    first commit

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models;
+
+[Table("Categorias")] //Não é necessário, pois isso já foi definido no applicationDbContext
+public class Categoria
+{
+    [Key] //Não é necessário, pois o EF define a propriedade com nome Id como PK
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(80)]
+    public string Nome { get; set; }
+
+    [Required]
+    [MaxLength(300)]
+    public string ImagemUrl { get; set; }
+
+    [Required]
+    [MaxLength(300)]
+    public ICollection<Produto> Produtos { get; set; }
+
+    public Categoria()
+    {
+        this.Produtos = new Collection<Produto>();
+    }
+}
+
